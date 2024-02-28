668d41708ca87a4a50f8cf06ae366598da6a88e2
Iker Urlezaga <iurlezaga@sisteplant.com>
2024-02-28 10:17:22 +0000

    use prettier

--- lua/ikerurle/lsp.lua
+++ lua/ikerurle/lsp.lua
@@ -1,6 +1,6 @@
 local ft = require('guard.filetype')
 
-ft('typescript,javascript,typescriptreact,html,yaml,vue'):fmt('prettier'):lint('eslint')
+ft('typescript,javascript,typescriptreact,html,yaml,vue'):fmt('prettier')
 
 require("guard").setup {
   fmt_on_save = false,
